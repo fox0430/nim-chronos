30af636d0d742854a8fa2d514b77338ad689aaf0
cheatfate <eugene.kabanov@status.im>
2022-04-12 04:35:58 +0300

    Some fixes.

--- chronos/ioselects/ioselectors_epoll.nim
+++ chronos/ioselects/ioselectors_epoll.nim
@@ -13,9 +13,8 @@
 
 import std/[posix, times, epoll]
 import stew/results
-import ../handles
 
-export results, times
+export results
 
 # Maximum number of events that can be returned
 const MAX_EPOLL_EVENTS = 64
@@ -318,7 +317,8 @@ proc registerTimer*[T](s: Selector[T], timeout: int, oneshot: bool,
 
 when not defined(android):
   proc registerSignal*[T](s: Selector[T], signal: int,
-                          data: T): Result[int, OSError] {.raises: [Defect].} =
+                          data: T): Result[int, OSErrorCode] {.
+       raises: [Defect].} =
     var
       nmask: Sigset
       omask: Sigset
@@ -350,7 +350,8 @@ when not defined(android):
     ok(fdi)
 
   proc registerProcess*[T](s: Selector, pid: int,
-                           data: T): Result[int, OSError] {.raises: [Defect].} =
+                           data: T): Result[int, OSErrorCode] {.
+       raises: [Defect].} =
     var
       nmask: Sigset
       omask: Sigset
