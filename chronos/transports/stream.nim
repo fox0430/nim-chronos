6cf3342cf38365df96ac687f2f632b50647704d3
cheatfate <eugene.kabanov@status.im>
2022-05-16 02:08:48 +0300

    Fix mistype.

--- chronos/transports/stream.nim
+++ chronos/transports/stream.nim
@@ -1651,8 +1651,7 @@ else:
     if ReadPaused in transp.state:
       ? addReader2(transp.fd, readStreamLoop, cast[pointer](transp))
       transp.state.excl(ReadPaused)
-    else:
-      ok()
+    ok()
 
   proc resumeWrite(transp: StreamTransport): Result[void, OSErrorCode] =
     if transp.queue.len() == 1:
